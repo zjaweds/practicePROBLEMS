390c7a5ecfbbb1b2dd69072491afcc01cd3d310b
Jawed Alam <zjaweds@outlook.com>
2020-10-14 16:02:45 +0530

    Pointer arithmetic implementation.

--- arraysPointersDynamicAllocation/ptr2DerivedType.cpp
+++ arraysPointersDynamicAllocation/ptr2DerivedType.cpp
@@ -29,13 +29,18 @@ int derived::getRooms(){
 }
 int main(){
     base *b;
-    derived d;
-    b=&d;
-    b->setFloor(4);
-    cout<<b->getFloor()<<"\n";
-    ((derived *)b)->setRooms(10);/*We can't access members of derived class directly 
+    derived d[3];
+    b=d;
+    d[0].setFloor(2);
+    d[1].setFloor(5);
+    //b->setFloor(4);
+    ((derived *)b)->setRooms(25);/*We can't access members of derived class directly 
     through pointer to the base class without typecasting*/
-    cout<<((derived *)b)->getRooms();
+    cout<<((derived *)b)->getRooms()<<endl;
+    cout<<b->getFloor();
+    b++;
+    cout<<endl<<b->getFloor();/*It is returning garbage value instead of 5 because after pointer increment it points
+    to the next object of the base class*/
 
     /*Apart from abovementioned pointer arithmetic occurs according to the base class
     that increment to the pointer causes it to point the next object of the base class 
