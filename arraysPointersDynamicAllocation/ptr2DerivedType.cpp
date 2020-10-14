736773f150ca1ff21e79b1b9c5a91a76c9651a90
Jawed Alam <zjaweds@outlook.com>
2020-10-14 15:48:04 +0530

    Arithmetic operations on pointers.

--- arraysPointersDynamicAllocation/ptr2DerivedType.cpp
+++ arraysPointersDynamicAllocation/ptr2DerivedType.cpp
@@ -36,5 +36,9 @@ int main(){
     ((derived *)b)->setRooms(10);/*We can't access members of derived class directly 
     through pointer to the base class without typecasting*/
     cout<<((derived *)b)->getRooms();
+
+    /*Apart from abovementioned pointer arithmetic occurs according to the base class
+    that increment to the pointer causes it to point the next object of the base class 
+    rather than the derived class therefore it creates problems if not handled properly*/
     return 0;
 }
