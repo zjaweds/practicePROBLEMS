2f42f1610ee10cfbe612724846c475057b9205cd
Jawed Alam <zjaweds@outlook.com>
2020-10-14 08:50:47 +0530

    Pointer assignment demonstrated what are the restriction over pointers assignment

--- arraysPointersDynamicAllocation/ptr2Obj.cpp
+++ arraysPointersDynamicAllocation/ptr2Obj.cpp
@@ -19,13 +19,41 @@ c1::c1(int i){
 int c1::getValue(){
     return value;
 }
+class c2{
+    int value;
+    public:
+    c2(int i);
+    int getValue();
+};
+c2::c2(int i){
+    value=i;
+}
+int c2::getValue(){
+    return value;
+}
 int main(){
-    c1 obj[]={31,13,17,19};
-    c1 *ptr;
-    ptr = obj;
+    c1 obj1[]={31,13,17,19};
+    c1 *ptr1,*ptr2;
+    ptr1 = obj1;
+    ptr2=ptr1;
+    c2 obj2[]={7,3,5,11};
+    c2 *ptr3,*ptr4;
+    ptr3 = obj2;
+    ptr4=ptr3;
+
+    //ptr4=ptr1; 
+    /*If we don't comment above statement it will cause
+    compilation error because pointers to same data type
+    can be assigned and as classes are abstract data type
+    same thing applies here. Although structure of both
+    of the classes are same but their name is different
+    therefore they are of different data types for the compiler.*/
     for(int i=0;i<4;i++){
-        cout<<ptr->getValue()<<"\t";
-        ptr++;
+        cout<<"C1:C1::C2:C2\t"<<ptr1->getValue()<<"\t"<<(ptr2+3)->getValue()<<"\t"<<ptr3->getValue()<<"\t"<<(ptr4+3)->getValue()<<endl;
+        ptr1++;
+        ptr2--;
+        ptr3++;
+        ptr4--;
     }
     return 0;
 }
