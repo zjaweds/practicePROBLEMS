fb29e58971093af914e876914b5f31aaa6b0117c
Jawed Alam <zjaweds@outlook.com>
2020-10-14 15:42:16 +0530

    Type casting for the access of members of derived class through pointer to the base class.

---
+++ arraysPointersDynamicAllocation/ptr2DerivedType.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+class base{
+    int floor;
+    public:
+    void setFloor(int i);
+    int getFloor(); 
+};
+void base::setFloor(int i){
+    floor=i;
+}
+int base::getFloor(){
+    return floor;
+}
+
+class derived:public base{
+    int rooms;
+    public:
+    void setRooms(int i);
+    int getRooms();
+};
+void derived::setRooms(int i){
+    rooms=i;
+}
+int derived::getRooms(){
+    return rooms;
+}
+int main(){
+    base *b;
+    derived d;
+    b=&d;
+    b->setFloor(4);
+    cout<<b->getFloor()<<"\n";
+    ((derived *)b)->setRooms(10);/*We can't access members of derived class directly 
+    through pointer to the base class without typecasting*/
+    cout<<((derived *)b)->getRooms();
+    return 0;
+}
