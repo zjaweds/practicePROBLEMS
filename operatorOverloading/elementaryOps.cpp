b8508c9d419b8854262630fa7f55396a4cba0a3f
Jawed Alam <zjaweds@outlook.com>
2020-10-15 16:25:54 +0530

    Elementary operator overloading demonstrated

---
+++ operatorOverloading/elementaryOps.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+
+using namespace std;
+
+class Quantity{
+    int weight;
+    int volume;
+    public:
+    void setValues(float w,float v){
+        weight=w;
+        volume=v;
+    }
+    void setValues(int w,int v){
+        weight=w;
+        volume=v;
+    }
+    void showValues(){
+        cout<<"Weight: "<<weight<<"\t Volume: "<<volume<<endl;
+    }
+    Quantity operator+(Quantity x){
+        Quantity temp;
+        temp.weight=weight+x.weight;
+        temp.volume=volume+x.volume;
+        return temp;
+    }
+    Quantity operator-(Quantity x){
+        Quantity temp;
+        temp.weight=weight-x.weight;
+        temp.volume=volume-x.volume;
+        return temp;
+    }
+    Quantity operator++(){
+        weight++;
+        volume++;
+        return *this;
+    }
+    Quantity operator=(Quantity x){
+        weight=x.weight;
+        volume=x.volume;
+        return *this;
+    }
+};
+
+int main(){
+    Quantity q1,q2;
+    q1.setValues(10,13);
+    q2.setValues(10,16);
+    (q1+q2).showValues();
+    (q1-q2).showValues();
+    (q1=q2).showValues();
+    (++q1).showValues();
+    return 0;
+}
