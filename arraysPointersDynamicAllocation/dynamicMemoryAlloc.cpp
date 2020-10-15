9f00bec6fdfa0dd79fb23469eecefe02a6446d90
Jawed Alam <zjaweds@outlook.com>
2020-10-15 08:07:56 +0530

    Dyanamic memory allocation

---
+++ arraysPointersDynamicAllocation/dynamicMemoryAlloc.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hell's this World!"<<endl;
+    int *p,*q,*ptrArray;
+    try{
+        p = new int;//Without initializer 
+        q= new int(30);//With initialisation
+        ptrArray=new int[5];//Array can't be initialised with declaration
+        cout<<"Memory allocated"<<endl;
+    }
+    catch(bad_alloc xa){
+        cout<<"Memory allocation failed"<<endl;
+    }
+    for(int i=0;i<4;i++){
+        *ptrArray=i;
+        cout<<"Array["<<i<<"]:"<<*ptrArray<<endl;
+        ptrArray++;
+    }
+
+    *p=10;
+    cout<<"P: "<<*p<<endl;
+    cout<<"Q: "<<*q<<endl<<endl;
+    delete p;
+    delete q;
+    delete ptrArray;
+    return 0;
+}
