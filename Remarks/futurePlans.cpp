9b4953f65ef0649a31e0dcc51f365979a9fdd62d
Jawed Alam <zjaweds@outlook.com>
2020-10-17 16:47:09 +0530

    Plans for exploration of more nuances of c++

---
+++ Remarks/futurePlans.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"More nuances to be explored."<<endl;
+    cout<<"And implemented later on.(:->"<<endl;
+    return EXIT_SUCCESS;
+}
