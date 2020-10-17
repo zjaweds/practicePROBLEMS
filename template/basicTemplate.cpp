194e8722235556072833d3b1de5ea38d57b6756d
Jawed Alam <zjaweds@outlook.com>
2020-10-17 09:33:23 +0530

    Several modifications and inclusion of purpose up to some extent

--- template/basicTemplate.cpp
+++ template/basicTemplate.cpp
@@ -2,27 +2,25 @@
 
 using namespace std;
 
-template <class crawlers> void sum(crawlers x, crawlers y){
-    cout<<(x+y)<<endl;
-}
-template <class crawlers,class flyers> void product(crawlers x, flyers y){
-    cout<<(x*y)<<endl;
-}
+template <class crawler> void sswap(crawler &x, crawler &y){
+    crawler temp;
 
-class crawlers{
-    int wheels;
-    protected:
-    int age;
-    public:
-    crawlers(int w,int a){
-        wheels=w;
-        age=a;
-    }
-};
-
+    temp=x;
+    x=y;
+    y=temp;
+ }
+ template <class X, class Y,class Z> void rverse(X a, Y b, Z c){//Overloading of << required in case of objects of classes
+     cout<<c<<b<<a<<endl;
+ }
 int main(){
-    sum(10,20);
-    product(10,20);
-    sum('a','b');
+    int i=13,j=31;
+    char a='A',b='B';
+    cout<<"Original: i: "<<i<<"\t j:"<<j<<endl;
+    sswap(i,j);
+    cout<<"Swapped: i: "<<i<<"\t j:"<<j<<endl;
+    cout<<"Original: a: "<<a<<"\t b:"<<b<<endl;
+    sswap(a,b);
+    cout<<"Swapped: a: "<<a<<"\t b:"<<b<<endl;
+    rverse(10,20,30);
     return 0;
-    }
+}
