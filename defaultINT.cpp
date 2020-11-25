6d5729dd62566e6782a1aec2cd8a5b47fa254512
Jawed Alam <zjaweds@outlook.com>
2020-11-25 15:27:30 +0530

    Changes made to reflect the purpose

--- defaultINT.cpp
+++ defaultINT.cpp
@@ -5,12 +5,17 @@ using namespace std;
 
 int readnum();
 main(){
-    printf("Hell's this World! %f",add(2,3));
-    //return EXIT_SUCCESS;
+    int num1,num2;
+    printf("Enter numbers");
+    num1=readnum();
+    num2=readnum();
+    printf("Hell's this World! %f",add(num1,num2));
+    return EXIT_SUCCESS;
 }
 
 int readnum(){/* if we do not put int before definition,
-as per standard C++ program would not be executed successfully*/
+as per standard C++ program would not be executed successfully but
+as of now it is fine without int too*/
     int n;    
     cout<<"\nEnter a number: ";
     cin>>n;
